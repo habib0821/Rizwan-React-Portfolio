1447082327bb14ab2037a253fc2799210d91056c
habib0821 <hu790238@gmail.com>
2025-05-23 14:29:22 -0700

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,152 @@
+export const Bio = {
+  name: "Rizwan Nadeem",
+  roles: [
+    "Graphic Designer",
+    "Poster Designer",
+    "UI/UX Designer",
+    "Thumbnail Designer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/rishavchanda",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Skills",
+    skills: [
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://images.seeklogo.com/logo-png/28/2/adobe-photoshop-cc-circle-logo-png_seeklogo-282949.png",
+      },
+      {
+        name: "Adobe Illustrator",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-adobe-illustrator-logo-icon-download-in-svg-png-gif-file-formats--brand-pack-business-icons-7581618.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://logos-world.net/wp-content/uploads/2024/10/Figma-Logo-New.png",
+      }
+    ],
+  }
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBBqhYHhYO5ju6yg9_QOKF1ciTivsqOV6XxQ&s",
+    role: "Graphic Designer",
+    company: "ZETTARUGS",
+    date: "Aug 2024 - July 2024",
+    desc: "Working on social media posts, Thumbnail designing and ads poster designing",
+    skills: [
+      "Photoshop",
+      "Illustrator",
+      "figma",
+    ],
+    doc: "https://cdn-ilcijib.nitrocdn.com/JZxAIeWXjbPitkYOLwVbzCvFkbBNKvbX/assets/images/optimized/rev-ba27ccf/zettarugs.com/wp-content/uploads/2024/09/Modern-Gabbah-2048x1638.jpg",
+  },
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSul6-qre1z_1YxzMdeWR00E_5UuCZcwlSj_g&s",
+    role: "Graphic Designer",
+    company: "BABAAZ TRAVELS",
+    date: "Aug 2024 - July 2025",
+    desc: "Working on social media posts, Thumbnail designing and ads poster designing",
+    skills: [
+      "Photoshop",
+      "Illustrator",
+      "figma",
+    ],
+    doc: "https://www.babaaztravels.com/wp-content/uploads/2022/05/ABOUTUS.jpg",
+  }
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  }
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/thumbnail (1).jpeg",
+    category: "thumbnail",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/thumbnail (2).jpeg",
+    category: "thumbnail",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/thumbnail (3).jpeg",
+    category: "thumbnail",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/thumbnail (4).jpeg",
+    category: "thumbnail",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/thumbnail (5).jpeg",
+    category: "thumbnail",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/poster (1).jpeg",
+    category: "poster",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/poster (2).jpeg",
+    category: "poster",
+  },
+  {
+    id: 0,
+    title: "Elegant Thumbnail",
+    image:
+      "/images/poster (3).jpeg",
+    category: "poster",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
